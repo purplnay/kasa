53ef0c5a42c39abd13f494a14c4d9217d8d450e9
purplnay <purplnay@gmail.com>
2024-04-12 14:34:08 +0200

    Add 404 page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,3 +1,16 @@
+import { Link } from "react-router-dom";
+import styles from "./NotFoundPage.module.scss";
+
 export default function NotFoundPage() {
-  return <h1>404</h1>
+  return (
+    <main className={styles.container}>
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.text}>
+        Oups! La page que vous demandez n&#39;existe pas.
+      </p>
+      <Link className={styles.link} to="/">
+        Retourner sur la page d&#39;accueil
+      </Link>
+    </main>
+  );
 }
