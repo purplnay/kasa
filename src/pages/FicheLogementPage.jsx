8d50f76e38070e67e289617a4dff2658f4c87bbf
purplnay <purplnay@gmail.com>
2024-04-12 14:21:15 +0200

    Add 404 if housing does not exist

--- src/pages/FicheLogementPage.jsx
+++ src/pages/FicheLogementPage.jsx
@@ -1,3 +1,16 @@
+import logements from "../assets/logements.json";
+import { useParams } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
+
 export default function FicheLogementPage() {
-  return <h1>Fiche logement</h1>
+  const { id } = useParams();
+  const logement = logements.find((logement) => logement.id === id);
+
+  // Diapo a des fleches et un numero
+
+  if (!logement) {
+    return <NotFoundPage />;
+  }
+
+  return <h1>Fiche logement</h1>;
 }
