223ef1f7f60234a61e2e0d33c9052ca6c1145b0c
purplnay <purplnay@gmail.com>
2024-04-26 15:16:36 +0200

    Add fiche logement

--- src/pages/FicheLogementPage.jsx
+++ src/pages/FicheLogementPage.jsx
@@ -1,6 +1,10 @@
 import logements from "../assets/logements.json";
 import { useParams } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
+import Carousel from "../components/Carousel";
+import Rating from "../components/Rating";
+import styles from "./FicheLogementPage.module.scss";
+import Collapse from "../components/Collapse";
 
 export default function FicheLogementPage() {
   const { id } = useParams();
@@ -12,5 +16,57 @@ export default function FicheLogementPage() {
     return <NotFoundPage />;
   }
 
-  return <h1>Fiche logement</h1>;
+  return (
+    <div>
+      <header>
+        <Carousel pictures={logement.pictures} title={logement.title} />
+
+        <div className={styles.header}>
+          <div className={styles.info}>
+            <div>
+              <h1 className={styles.title}>{logement.title}</h1>
+              <p className={styles.location}>{logement.location}</p>
+            </div>
+            <ul className={styles.tagsList}>
+              {logement.tags.map((tag) => (
+                <li key={tag} className={styles.tagItem}>
+                  <span>{tag}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.hostAndRating}>
+            <div className={styles.host}>
+              <p className={styles.hostName}>{logement.host.name}</p>
+              <img
+                className={styles.hostPicture}
+                src={logement.host.picture}
+                alt={`Photo de ${logement.host.name}`}
+              />
+            </div>
+            <div>
+              <Rating value={logement.rating} />
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <section className={styles.content}>
+        <article className={styles.contentArticle}>
+          <Collapse title={<h2>Description</h2>}>
+            <p>{logement.description}</p>
+          </Collapse>
+        </article>
+        <article className={styles.contentArticle}>
+          <Collapse title={<h2>Équipements</h2>}>
+            <ul className={styles.equipmentList}>
+              {logement.equipments.map((equipement) => (
+                <li key={equipement}>{equipement}</li>
+              ))}
+            </ul>
+          </Collapse>
+        </article>
+      </section>
+    </div>
+  );
 }
